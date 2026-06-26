f48cef7a5762eecfc066c3e04f4af5251c0e7d25
Debayo1 <debayo@users.noreply.github.com>
2026-06-26 16:09:20 +0100

    chore: configure root redirect in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.199"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
