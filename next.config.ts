bb7230b3221aab9623d088bc96b6d02db18fe236
Debayo1 <debayo@users.noreply.github.com>
2026-06-28 01:22:51 +0100

    chore: allow 192.168.0.200 dev origin in config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.199"],
+  allowedDevOrigins: ["192.168.0.200", "192.168.0.199", "localhost"],
   async redirects() {
     return [
       {
