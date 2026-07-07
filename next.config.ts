da725be6999849b2c3c98de10591d14336f02611
Debayo1 <debayo@users.noreply.github.com>
2026-07-07 18:23:07 +0100

    config: add port suffix versions to allowedDevOrigins in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.198", "192.168.0.200", "192.168.0.199", "localhost"],
+  allowedDevOrigins: [
+    "192.168.0.198", "192.168.0.198:3000",
+    "192.168.0.200", "192.168.0.200:3000",
+    "192.168.0.199", "192.168.0.199:3000",
+    "localhost", "localhost:3000"
+  ],
 };
 
 export default nextConfig;
